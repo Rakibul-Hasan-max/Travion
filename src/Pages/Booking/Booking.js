16539c57a598bf384450d5733c514d63aeef736d
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-10-31 19:59:00 +0600

    almost

--- src/Pages/Booking/Booking.js
+++ src/Pages/Booking/Booking.js
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
+import useAuth from '../../Hooks/useAuth';
+import './Booking.css';
 
 const Booking = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const {user} = useAuth();
+    const onSubmit = data => {console.log(data)};
+
     const { serviceId } = useParams();
     const [service, setService] = useState({});
 
@@ -12,44 +19,20 @@ const Booking = () => {
     }, [])
 
 
-
     return (
         <div>
-            <h2>Details of: {service.name}</h2>
+            <h2>Details of:</h2>
             <h2>This is booking: {serviceId}</h2>
+            <form className="booking-form" onSubmit={handleSubmit(onSubmit)}>
+                 <input placeholder="Name" defaultValue={user.displayName} {...register("name")} />                 <input placeholder="Email" defaultValue={user.email} {...register("email", { required: true })} />
+                 {errors.email && <span className="error">This field is required</span>}
+                 <input placeholder="Phone number" defaultValue="" {...register("phone")} />
+                 <input placeholder="Address" defaultValue="" {...register("address")} />
+                <input placeholder="Zip code" defaultValue="" {...register("zip code")} />
+                <input className="submit" type="submit" />
+             </form>
         </div>
     );
 };
 
 export default Booking;
-
-
-
-// import React from 'react';
-// import { useForm } from 'react-hook-form';
-// import useAuth from '../../hooks/useAuth';
-// import './Shipping.css'
-
-// const Shipping = () => {
-
-//     const { register, handleSubmit, formState: { errors } } = useForm();
-
-//     const {user} = useAuth();
-//     const onSubmit = data => {console.log(data)};
-
-//     return (
-//         <div>
-//             <form className="shipping-form" onSubmit={handleSubmit(onSubmit)}>
-//                 <input placeholder="Name" defaultValue={user.displayName} {...register("name")} />
-//                 <input placeholder="Email" defaultValue={user.email} {...register("email", { required: true })} />
-//                 {errors.email && <span className="error">This field is required</span>}
-//                 <input placeholder="Phone number" defaultValue="" {...register("phone")} />
-//                 <input placeholder="Address" defaultValue="" {...register("address")} />
-//                 <input placeholder="Zip code" defaultValue="" {...register("zip code")} />
-//                 <input className="submit" type="submit" />
-//             </form>
-//         </div>
-//     );
-// };
-
-// export default Shipping;
